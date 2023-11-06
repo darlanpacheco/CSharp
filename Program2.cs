4287f0618e287bf35a4047ffe2399f5c15cf9274
Darlan Pacheco <darlanpacheco@proton.me>
2023-11-06 18:23:37 -0300

    C# inheritance

--- Program2.cs
+++ Program2.cs
@@ -1,21 +1,50 @@
-class DestructorDemo
+class Destructor
 {
-    public DestructorDemo()
-    {
-        Console.WriteLine("Constructor Object Created");
-    }
-    ~DestructorDemo()
-    {
-        string type = GetType().Name;
-        Console.WriteLine($"Object {type} is Destroyed");
-    }
+  public Destructor()
+  {
+    Console.WriteLine("Constructor Object Created");
+  }
+  ~Destructor()
+  {
+    string type = GetType().Name;
+    Console.WriteLine($"Object {type} is Destroyed");
+  }
+}
+
+class Vehicle // Base class
+{
+  public int maxVelocity, wheels;
+  public bool isTurned;
+
+  public void turnOn()
+  {
+    isTurned = true;
+  }
+
+  public void turnOff()
+  {
+    isTurned = false;
+  }
+}
+
+class Car : Vehicle
+{
+  string name = "", color = "";
+
+  public Car(string name, string color)
+  {
+    wheels = 4;
+    maxVelocity = 120;
+    this.name = name;
+    this.color = color;
+    turnOff();
+  }
 }
 
 class Program2
 {
-    static void Main()
-    {
-        DestructorDemo obj1 = new DestructorDemo();
-        DestructorDemo obj2 = new DestructorDemo();
-    }
+  static void Main()
+  {
+    Car c1 = new Car("Fast", "Red");
+  }
 }

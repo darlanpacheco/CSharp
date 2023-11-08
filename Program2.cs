51163871f0048e5c65028ffdf59b9681cf2ff846
Darlan Pacheco <darlanpacheco@proton.me>
2023-11-07 22:03:56 -0300

    Base construtor.

--- Program2.cs
+++ Program2.cs
@@ -13,8 +13,14 @@ class Destructor
 
 class Vehicle // Base class
 {
-  public int maxVelocity, wheels;
+  public int maxVelocity = 0;
+  private int wheels = 0;
   public bool isTurned;
+
+  public Vehicle(int wheels)
+  {
+    this.wheels = wheels;
+  }
 
   public void turnOn()
   {
@@ -25,19 +31,55 @@ class Vehicle // Base class
   {
     isTurned = false;
   }
+
+  public void status()
+  {
+    Console.WriteLine($"MaxVel: {maxVelocity}\nWheels: {getWheels()}\nTurned: {isTurned}");
+  }
+
+  public int getWheels()
+  {
+    return this.wheels;
+  }
+
+  public void setWheels(int value)
+  {
+    if (value < 0)
+    {
+      this.wheels = 0;
+    }
+    else if (value > 25)
+    {
+      this.wheels = 25;
+    }
+    else
+    {
+      this.wheels = value;
+    }
+  }
+
 }
 
 class Car : Vehicle
 {
   string name = "", color = "";
 
-  public Car(string name, string color)
+  public Car(string name, string color) : base(0)
   {
-    wheels = 4;
     maxVelocity = 120;
     this.name = name;
     this.color = color;
     turnOff();
+  }
+}
+
+class CombatCar : Car
+{
+  public int ammo = 0;
+  public CombatCar() : base("Combat Car", "Green")
+  {
+    setWheels(6);
+    ammo = 100;
   }
 }
 
@@ -46,5 +88,9 @@ class Program2
   static void Main()
   {
     Car c1 = new Car("Fast", "Red");
+    c1.turnOn();
+    c1.status();
+
+    CombatCar cc1 = new CombatCar();
   }
 }
